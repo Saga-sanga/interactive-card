829f7cc6d9c571ee19064216548f3f316236c124
Saga-sanga <vulcan248@gmail.com>
2022-08-11 22:10:31 +0530

    changed letter spacing of site font

--- src/components/CardDetailForm.js
+++ src/components/CardDetailForm.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function CardDetailForm() {
   return (
     <section className="basis-2/3 flex justify-center items-center">
-      <form className="flex flex-col gap-y-4 font-medium">
+      <form className="flex flex-col gap-y-4 font-semibold">
         <fieldset className="flex flex-col gap-y-1">
           <label htmlFor="holderName" className="text-xxs">CARDHOLDER NAME</label>
           <input 
-            className="placeholder:text-light-grayish-violet placeholder:font-medium placeholder:text-xs border border-solid rounded border-light-grayish-violet px-3 py-2" 
+            className="placeholder:text-light-grayish-violet placeholder:font-medium text-[14px] border border-solid rounded border-light-grayish-violet px-3 py-2" 
             id="holderName" 
             placeholder="e.g. Jane Appleseed"
             type="text" 
@@ -16,7 +16,7 @@ export default function CardDetailForm() {
         <fieldset className="flex flex-col gap-y-1">
           <label htmlFor="cardNo" className="text-xxs">CARD NUMBER</label>
           <input 
-            className="placeholder:text-light-grayish-violet placeholder:text-xs border border-solid rounded border-light-grayish-violet px-3 py-2" 
+            className="placeholder:text-light-grayish-violet placeholder:font-medium text-[14px] border border-solid rounded border-light-grayish-violet px-3 py-2" 
             id="cardNo" 
             placeholder="e.g. 1234 5678 9123 0000"
             type="text" 
