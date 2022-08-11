67592db19058c2c97cc510291248d0c5a7c42e9b
Saga-sanga <vulcan248@gmail.com>
2022-08-11 22:29:30 +0530

    altered font sizes

--- src/components/CardDetailForm.js
+++ src/components/CardDetailForm.js
@@ -3,20 +3,20 @@ import React from "react";
 export default function CardDetailForm() {
   return (
     <section className="basis-2/3 flex justify-center items-center">
-      <form className="flex flex-col gap-y-4 font-semibold">
+      <form className="flex flex-col gap-y-4 w-2/5 font-semibold">
         <fieldset className="flex flex-col gap-y-1">
-          <label htmlFor="holderName" className="text-xxs">CARDHOLDER NAME</label>
+          <label htmlFor="holderName" className="text-xs">CARDHOLDER NAME</label>
           <input 
-            className="placeholder:text-light-grayish-violet placeholder:font-medium text-[14px] border border-solid rounded border-light-grayish-violet px-3 py-2" 
+            className="placeholder:text-light-grayish-violet placeholder:font-medium text-sm border border-solid rounded border-light-grayish-violet px-3 py-2" 
             id="holderName" 
             placeholder="e.g. Jane Appleseed"
             type="text" 
           />
         </fieldset>
         <fieldset className="flex flex-col gap-y-1">
-          <label htmlFor="cardNo" className="text-xxs">CARD NUMBER</label>
+          <label htmlFor="cardNo" className="text-xs">CARD NUMBER</label>
           <input 
-            className="placeholder:text-light-grayish-violet placeholder:font-medium text-[14px] border border-solid rounded border-light-grayish-violet px-3 py-2" 
+            className="placeholder:text-light-grayish-violet placeholder:font-medium text-sm border border-solid rounded border-light-grayish-violet px-3 py-2" 
             id="cardNo" 
             placeholder="e.g. 1234 5678 9123 0000"
             type="text" 
