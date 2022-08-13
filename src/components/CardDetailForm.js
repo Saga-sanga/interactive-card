ac4e58eace0ccd0b2e57b3d88a997f11f943add2
Saga-sanga <vulcan248@gmail.com>
2022-08-13 19:37:40 +0530

    added more form elements

--- src/components/CardDetailForm.js
+++ src/components/CardDetailForm.js
@@ -7,21 +7,50 @@ export default function CardDetailForm() {
         <fieldset className="flex flex-col gap-y-1">
           <label htmlFor="holderName" className="text-xs">CARDHOLDER NAME</label>
           <input 
-            className="placeholder:text-light-grayish-violet placeholder:font-medium text-sm border border-solid rounded border-light-grayish-violet px-3 py-2" 
+            className="placeholder:text-light-grayish-violet placeholder:font-medium text-sm border border-solid rounded border-light-grayish-violet p-3" 
             id="holderName" 
             placeholder="e.g. Jane Appleseed"
             type="text" 
           />
         </fieldset>
+
         <fieldset className="flex flex-col gap-y-1">
           <label htmlFor="cardNo" className="text-xs">CARD NUMBER</label>
           <input 
-            className="placeholder:text-light-grayish-violet placeholder:font-medium text-sm border border-solid rounded border-light-grayish-violet px-3 py-2" 
+            className="placeholder:text-light-grayish-violet placeholder:font-medium text-sm border border-solid rounded border-light-grayish-violet p-3" 
             id="cardNo" 
             placeholder="e.g. 1234 5678 9123 0000"
             type="text" 
           />
         </fieldset>
+
+        <div className="flex flex-row">
+          <fieldset className="flex flex-col gap-y-1 w-1/2">
+            <label htmlFor="">EXP. DATE (MM/YY)</label>
+            <div className="flex flex-row">
+              <input
+                className="flex-1 placeholder:text-light-grayish-violet placeholder:font-medium text-sm border border-solid rounded border-light-grayish-violet p-3"
+                placeholder="MM"
+                type="text"
+              />
+              <input
+                className="flex-1 placeholder:text-light-grayish-violet placeholder:font-medium text-sm border border-solid rounded border-light-grayish-violet p-3"
+                placeholder="YY"
+                type="text" 
+              />
+            </div>
+          </fieldset>
+
+          <fieldset className="flex flex-col gap-y-1 w-1/2">
+            <label htmlFor="">CVC</label>
+            <input
+              className="placeholder:text-light-grayish-violet placeholder:font-medium text-sm border border-solid rounded border-light-grayish-violet p-3"
+              type="text" 
+              placeholder="e.g. 123"
+            />
+          </fieldset>
+        </div>
+        <button className="bg-very-dark-violet text-white rounded py-3">Confirm</button>
       </form>
     </section>
   )
