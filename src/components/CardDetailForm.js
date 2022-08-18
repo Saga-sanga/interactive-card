bd1cffbb7ab39a14ac1c4660a4aad44b07244038
Saga-sanga <vulcan248@gmail.com>
2022-08-18 21:33:33 +0530

    did somethings

--- src/components/CardDetailForm.js
+++ src/components/CardDetailForm.js
@@ -2,17 +2,31 @@ import React from "react";
 import Cleave from "cleave.js/react";
 
 export default function CardDetailForm({onSubmitButton, setUserName, setCardNo, setCardMonth, setCardYear, setCVC}) {
-  const validateInput = (e) => /\D\s/g.test(e.target.value) || e.target.value === ''? 
-    e.currentTarget.classList.add('border-red-error'):
-    e.currentTarget.classList.remove('border-red-error');
+  const errorMessage = "Can't be blank";
+  
+  const validateInput = (e) => (/[a-z]/ig.test(e.target.value) === true) || (e.target.value === '')? 
+    e.currentTarget.classList.add('red-error-border'):
+    e.currentTarget.classList.remove('red-error-border');
+
+  const checkError = (e) => {
+    e.preventDefault();
+    console.log(e.target.same)
+  }
   
   return (
     <section className="flex-[2_1_66.666667%] flex justify-center items-center">
-      <form className="flex flex-col gap-y-4 w-2/5 font-semibold ml-40">
+      <form 
+        className="flex flex-col gap-y-4 w-2/5 font-semibold ml-40"
+        onSubmit={checkError}
+      >
         <fieldset className="flex flex-col gap-y-1">
           <label htmlFor="holderName" className="text-xs">CARDHOLDER NAME</label>
           <input 
-            onChange={(e) => setUserName(e.target.value)}
+            onChange={(e) => {
+              setUserName(e.target.value);
+              e.target.value === "" ? e.currentTarget.classList.add('red-error-border'):
+              e.currentTarget.classList.remove('red-error-border');
+            }}
             className="
               placeholder:text-light-grayish-violet 
               placeholder:font-medium 
@@ -22,8 +36,10 @@ export default function CardDetailForm({onSubmitButton, setUserName, setCardNo, 
             id="holderName" 
             placeholder="e.g. Jane Appleseed"
             type="text"
-            maxLength={30} 
+            maxLength={30}
+            name="same"
           />
+          <span className="text-[12px] text-red-error tracking-normal">{errorMessage}</span>
         </fieldset>
 
         <fieldset className="flex flex-col gap-y-1">
@@ -102,7 +118,7 @@ export default function CardDetailForm({onSubmitButton, setUserName, setCardNo, 
         <button 
           type="submit"
           className="bg-very-dark-violet hover:bg-violet-900 text-white rounded-lg py-3 mt-4"
-          onClick={onSubmitButton}
+          // onClick={onSubmitButton}
         >Confirm</button>
       </form>
     </section>
